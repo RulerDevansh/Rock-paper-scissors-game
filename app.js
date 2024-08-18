269abce710566434ef3b897d5488fe2766b791d4
Devansh <138721175+RulerDevansh@users.noreply.github.com>
2024-08-18 20:45:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+// ROCK PAPER SISSOR GAME
+let US=document.querySelector('#player_score');//INITIAL IS SET 0 IN HTML
+let CS=document.querySelector('#computer_score');
+
+let choices = document.querySelectorAll('.choice');
+let usrChoosen = document.querySelector('#player_choosen');
+let compChoosen = document.querySelector('#computer_choosen');
+let message = document.querySelector('.message');
+
+function getCompChoice() {
+    let c = ["rock","paper","sissors"];
+    let i = Math.floor(Math.random()*3);//Math.random():-Generates no. from 0-1 eg 0.3543.. So to get upto 2 multiply by 3 the take floor.
+    return c[i];
+}
+function play(usrC,compC) {
+    if (usrC==compC) {
+        return "draw";
+    }
+    else if (usrC=="rock" && compC=="paper") {
+        return "computer";
+    }
+    else if (usrC=="paper" && compC=="sissors") {
+        return "computer";
+    }
+    else if (usrC=="sissors" && compC=="rock") {
+        return "computer";
+    }
+    else return "user";
+}
+
+choices.forEach((c)=>{
+    c.addEventListener("click",()=>{
+        let id = c.getAttribute('id');
+        let compC = getCompChoice();
+        // For changing choosen background
+        let str="";
+        if (window.innerWidth>700) {
+            str=`url(/img/${id}.png)`; 
+        }
+        else  str=`url(/img/${id}1.png)`; 
+        usrChoosen.style.backgroundImage = str;
+        if (window.innerWidth>700) {
+            str=`url(/img/${compC}.png)`; 
+        }
+        else  str=`url(/img/${compC}1.png)`;
+        compChoosen.style.backgroundImage = str;
+        let result = play(id,compC);
+        if (result=="draw") {
+            message.innerText="Oops!! Round Draw \nChoose Again!"; // updating message
+        }
+        else if(result=="user"){
+            US.innerText++;
+            message.innerText="YOU WON !!";
+        }
+        else {
+            CS.innerText++;
+            message.innerText="You LOST !!";
+        }
+    })
+});
